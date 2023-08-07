c1a19c74ea25d5db63644551c2518e2f9c8ae4e5
amsamuels <amsamuels21@gmail.com>
2023-08-07 06:29:08 +0100

    Insert into db fully functional & retriving from db working

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 	"time"
 
@@ -21,38 +23,60 @@ func NewServer(client *mongo.Client) *Server {
 	}
 }
 
+func (s *Server) handlergetFact(w http.ResponseWriter, r *http.Request) {
+	coll := s.client.Database("catfact").Collection("fact")
+
+	query := bson.M{}
+	cursor, err := coll.Find(context.TODO(), query)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	results := []bson.M{}
+	if err = cursor.All(context.TODO(), &results); err != nil {
+		log.Fatal(err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(results)
+
+}
+
 type CatFactWorker struct {
 	client *mongo.Client
 }
 
-func NewCatFactWorker(client *mongo.Client) *CatFactWorker {
+func NewCatFactWorker(c *mongo.Client) *CatFactWorker {
 	return &CatFactWorker{
-		client: client,
+		client: c,
 	}
 }
 
 func (cfw *CatFactWorker) start() error {
-	coll := cfw.client.Database("catfact").Collection("facts")
+	coll := cfw.client.Database("catfact").Collection("fact")
 	ticker := time.NewTicker(2 * time.Second)
 
 	i := 0
 	for i < 10 {
 		resp, err := http.Get("https://catfact.ninja/fact")
 		if err != nil {
-			return err
+			return fmt.Errorf("error getting cat fact: %v", err)
 		}
 		defer resp.Body.Close()
 
 		var catFact bson.M
 		err = json.NewDecoder(resp.Body).Decode(&catFact)
+		fmt.Println("Cat Fact: ", catFact)
 		if err != nil {
-			return err
+			return fmt.Errorf("error decoding cat fact: %v", err)
 		}
 		_, err = coll.InsertOne(context.TODO(), catFact)
 		if err != nil {
-			return err
+			return fmt.Errorf("error inserting cat fact: %v", err)
 		}
-
+		fmt.Println("Inserted document: ", catFact)
+		fmt.Println("Iteration: ", i)
 		<-ticker.C
 		i++
 	}
@@ -61,12 +85,16 @@ func (cfw *CatFactWorker) start() error {
 }
 
 func main() {
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:2717"))
 	if err != nil {
 		panic(err)
 	}
 
 	worker := NewCatFactWorker(client)
+	fmt.Println("Starting worker", worker)
 	go worker.start()
 
+	server := NewServer(client)
+	http.HandleFunc("/facts", server.handlergetFact)
+	http.ListenAndServe(":3000", nil)
 }
